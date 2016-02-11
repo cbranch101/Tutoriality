e0c2a75bbba80c0eef123b44fc78795a163f3876
Clay Branch <clay.branch@gmail.com>
2016-02-11 16:03:13 -0600

    first working front end test

--- test/client/components/MyComponent_test.js
+++ test/client/components/MyComponent_test.js
@@ -1,12 +1,13 @@
 require(TEST_HELPER) // <--- This must be at the top of every test file.
 
 var MyComponent = require(__client + '/components/MyComponent');
+var mq = require('mithril-query');
 
 describe("The MyComponent component", function() {
   it("does stuff'", function() {
   	var output = MyComponent.view(null, {title : "Test"});
-  	//
-  	
+    var $output = mq(output);
+    $output.find('h2')[0].children[0].should.equal("Test");
     //yied Tutorial.insert() * 2
 
     // Notice how we're in a generator function (indicated by the the *)
